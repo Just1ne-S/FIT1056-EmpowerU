92e8b0a3ac468a15282cac7964fd7f4b7e2fbd6e
Justin <114224750+Just1ne-S@users.noreply.github.com>
2024-09-29 02:29:04 +1000

    Added the main window for the application
    This will be the original window for the application, so the main master will be this interface

---
+++ EmpowerU Code/interfaces/main_window.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+
+from interfaces.homepage import HomePage
+
+class MW(tk.Tk):
+    
+    def __init__(self, title, width, height):
+        """
+        Constructor for the MSMS class.
+
+        Parameter(s):
+        - title: str
+        - width: int, width of window in pixels
+        - height: int, height of window in pixels
+        """
+        super().__init__()
+        super().title(title)
+        super().geometry(f"{width}x{height}")
+
+        self.homepage = HomePage(master=self, image_path="./images/EmpowerU logo.png")
+        self.show_homepage()
+
+    def show_homepage(self):
+        self.homepage.place(relx=.5, rely=.5, anchor=tk.CENTER)
+
+    def hide_homepage(self):
+        self.homepage.place_forget()
