2594f435e893592aa348f923191767889cdc70d7
Justin <114224750+Just1ne-S@users.noreply.github.com>
2024-09-29 02:24:35 +1000

    Update User.py
    Added the function of authentication to check whether the username matches the password

--- EmpowerU Code/app/User.py
+++ EmpowerU Code/app/User.py
@@ -1,4 +1,21 @@
+import os
 class User():
+    @staticmethod
+    def authenticate(input_username,input_password):
+        path = "./dat/login_info.txt"
+        delimiter = ","
+        if os.path.exists(path):
+            with open(path,"r") as rp:
+                lines = rp.readlines()
+            for line in lines:
+                first_name,last_name,username,password = line.split(delimiter)
+            if input_username == username and input_password == password:
+                return User(first_name,last_name,username,password)
+            else:
+                return "Incorrect Username or Password"
+        else:
+            return f"Please check subdirectory and file {path} exists."
+
     def __init__(self,first_name,last_name,username,password):
         self.first_name = first_name
         self.last_name = last_name
