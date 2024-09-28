5d57fdc7742c17f70c1a5519ddc8d301c3db4237
Justin <114224750+Just1ne-S@users.noreply.github.com>
2024-09-29 02:29:45 +1000

    main.py

--- EmpowerU Code/main.py
+++ EmpowerU Code/main.py
@@ -1,1 +1,10 @@
+import tkinter as tk
+from interfaces.main_window import MW
 
+def main():
+    root = MW(title="EmpowerU - Learning Application",width="950",height="700")
+    root.mainloop()
+    print("EmpowerU proper shutdown completed.")
+
+if __name__ == "__main__":
+    main()
