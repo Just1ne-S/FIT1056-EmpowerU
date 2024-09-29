71a0f93f51c8057dc28d04f2bd85c273b718b6cc
Justin <114224750+Just1ne-S@users.noreply.github.com>
2024-09-29 20:23:41 +1000

    Update User.py
    Added minor changes to the code

--- EmpowerU Code/app/User.py
+++ EmpowerU Code/app/User.py
@@ -2,22 +2,26 @@ import os
 class User():
     @staticmethod
     def authenticate(input_username,input_password):
-        path = "./dat/login_info.txt"
+        path = "./data/login_info.txt"
         delimiter = ","
         if os.path.exists(path):
             with open(path,"r") as rp:
                 lines = rp.readlines()
             for line in lines:
-                first_name,last_name,username,password = line.split(delimiter)
+                first_name,last_name,contact_num,username,password = line.split(delimiter)
             if input_username == username and input_password == password:
                 return User(first_name,last_name,username,password)
             else:
-                return "Incorrect Username or Password"
+                return None
         else:
-            return f"Please check subdirectory and file {path} exists."
+            print(f"Please check subdirectory and file {path} exists.")
 
     def __init__(self,first_name,last_name,username,password):
         self.first_name = first_name
         self.last_name = last_name
         self.username = username
         self.password = password
+    
+    
+if __name__ == "__main__":
+    pass
