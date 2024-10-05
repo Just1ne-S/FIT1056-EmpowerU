a09222d9c2070d6145c9ca905ffe3d95b3369677
Justin <114224750+Just1ne-S@users.noreply.github.com>
2024-10-06 03:52:07 +1100

    Update progress_bar.py

--- EmpowerU Code/interfaces/progress_bar.py
+++ EmpowerU Code/interfaces/progress_bar.py
@@ -2,18 +2,18 @@ import tkinter as tk
 from tkinter import ttk
 
 class ProgressBar(tk.Frame):
-    def __init__(self):
-        super().__init__()
-        self.progress_value = tk.DoubleVar(master=self)
-        self.progress_bar = ttk.Progressbar(self,variable=self.progress_value)
-    
+    def __init__(self,master):
+        super().__init__(master)
+        self.master = master
+        self.progress_value = tk.DoubleVar(value=0)  # Start from 0 for demonstration
+        self.progress_bar = ttk.Progressbar(self.master, variable=self.progress_value, length=850, maximum=100)
+        self.progress_bar.grid(row=1, padx=10, pady=10)
+
     def increase_bar(self):
         if self.progress_value.get() < 100:
             self.progress_value.set(self.progress_value.get() + 1)
-            self.progress_bar.set(self.progress_value)
         else:
             pass
-
+        
 if __name__ == "__main__":
     pass
-            
