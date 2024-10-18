ec998c21c519da4425b51b8bc79bd346cff5b8c4
Justin <114224750+Just1ne-S@users.noreply.github.com>
2024-10-18 23:05:03 +1100

    Update main.py

--- EmpowerU Code/main.py
+++ EmpowerU Code/main.py
@@ -1,7 +1,10 @@
 import tkinter as tk
 from interfaces.main_window import MW
+from app.adjust_lines import adjust_lines
 
 def main():
+    adjust_lines("./data/receptionist_login_info.txt")
+    adjust_lines("./data/user_login_info.txt")
     root = MW(title="EmpowerU - Learning Application",width="950",height="700")
     root.mainloop()
     print("EmpowerU proper shutdown completed.")
