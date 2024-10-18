febfbcd95fbc35fa45096dfeead61eafbcefbffa
Justin <114224750+Just1ne-S@users.noreply.github.com>
2024-10-19 00:47:50 +1100

    Update adjust_lines.py

--- EmpowerU Code/app/adjust_lines.py
+++ EmpowerU Code/app/adjust_lines.py
@@ -1,9 +1,5 @@
 def adjust_lines(path):
     rf = open(path,"r")
-    line = rf.readline()
-    condition = False
-    if line[-1] == "\n":
-        condition = True
-    if condition == True:
-        wf = open(path,"w")
-        wf.write(line[0:-1])
+    line = rf.read().strip()
+    wf = open(path,"w")
+    wf.write(line)
