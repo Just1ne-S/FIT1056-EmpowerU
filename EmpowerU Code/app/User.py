a61e2cd751bb195303d8b41bb98dc2b8fad891a8
Justin <114224750+Just1ne-S@users.noreply.github.com>
2024-10-19 19:22:32 +1100

    Update User.py

--- EmpowerU Code/app/User.py
+++ EmpowerU Code/app/User.py
@@ -7,20 +7,21 @@ class User():
             with open(path,"r") as rp:
                 lines = rp.readlines()
             for line in lines:
-                user_id,first_name,last_name,contact_num,username,password = line.strip().split(delimiter)
+                user_id,first_name,last_name,contact_num,username,password,code = line.strip().split(delimiter)
                 if input_username == username and input_password == password:
-                    return User(user_id,first_name,last_name,username,password)
+                    return User(user_id,first_name,last_name,username,password,code)
             else:
                 return None
         else:
             print(f"Please check subdirectory and file {path} exists.")
 
-    def __init__(self,user_id,first_name,last_name,username,password):
+    def __init__(self,user_id,first_name,last_name,username,password,code):
         self.user_id = user_id
         self.first_name = first_name
         self.last_name = last_name
         self.username = username
         self.password = password
+        self.code = code
     
     
 if __name__ == "__main__":
