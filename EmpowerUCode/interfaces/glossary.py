ca78200849a8b0482a48b7d30c468c8d0ae8d95d
mukundashiyamalan <mukundashiyamalan05@gmail.com>
2024-10-21 12:15:45 +1100

    Add files via upload

---
+++ EmpowerUCode/interfaces/glossary.py
@@ -0,0 +1,44 @@
+#glossary
+def glossary():
+    """
+    glossary function includes some definitions and terms related to python, artificial intelligence and information security
+    """
+    #source : https://www.w3schools.com/python/python_ref_glossary.asp
+    print("GLOSSARY\n\
+    Python\n\
+        Indentation - Indentation refers to the spaces at the beginning of a code line\n\
+        Comments - Comments are code lines that will not be executed\n\
+        Multiline Comments - Insert comments on multiple lines\n\
+        Int - The integer number type\n\
+        Float - The floating number type\n\
+        Complex - The complex number type\n\
+        Operators - Use operator to perform operations in Python\n\
+        Arithmetic Operators - Arithmetic operator are used to perform common mathematical operations\n\
+        Assignment Operators - Assignment operators are use to assign values to variables")
+    #source : https://www.sans.org/security-resources/glossary-of-terms/
+    print("    Information Security\n\
+        Access Control - Access Control ensures that resources are only granted to those users who are entitled to them.\n\
+        Access Control List (ACL) - A mechanism that implements access control for a system resource by listing the identities of the system entities that are permitted to access the resource.\n\
+        Access Control Service - A security service that provides protection of system resources against unauthorized access. The two basic mechanisms for implementing this service are ACLs and tickets.\n\
+        Access Management Access - Management is the maintenance of access information which consists of four tasks: account administration, maintenance, monitoring, and revocation.\n\
+        Cache - Pronounced cash, a special high-speed storage mechanism. It can be either a reserved section of main memory or an independent high-speed storage device. Two types of caching are commonly used in personal computers: memory caching and disk caching.\n\
+        Cache Cramming - Cache Cramming is the technique of tricking a browser to run cached Java code from the local disk, instead of the internet zone, so it runs with less restrictive permissions.\n\
+        Chain of Custody - Chain of Custody is the important application of the Federal rules of evidence and its handling.\n\
+        Inference Attack - Inference Attacks rely on the user to make logical connections between seemingly unrelated pieces of information.\n\
+        Internet Protocol (IP) - The method or protocol by which data is sent from one computer to another on the Internet.\n\
+        IP Address - A computer's inter-network address that is assigned for use by the Internet Protocol and other protocols. An IP version 4 address is written as a series of four 8-bit numbers separated by periods.")
+    #source : https://www.expert.ai/glossary-of-ai-terms/
+    print("    Artificial Intelligence\n\
+        Actionable Intelligence - Information you can leverage to support decision making.\n\
+        Annotation - The process of tagging language data by identifying and flagging grammatical, semantic or phonetic elements in language data.\n\
+        Composite AI - The combined application of different AI techniques to improve the efficiency of learning in order to broaden the level of knowledge representations and, ultimately, to solve a wider range of business problems in a more efficient manner.\n\
+        Data Drift - Data Drift occurs when the distribution of the input data changes over time; this is also known as covariate shift.\n\
+        Edge model - A model that includes data typically outside centralized cloud data centers and closer to local devices or individuals — for example, wearables and Internet of Things (IoT) sensors or actuators.\n\
+        ETL (Entity Recognition, Extraction) - Entity extraction is an NLP function that serves to identify relevant entities in a document.\n\
+        Generative AI (GenAI) - AI techniques that learn from representations of data and model artifacts to generate new artifacts.\n\
+        Hybrid AI - Hybrid AI is any artificial intelligence technology that combines multiple AI methodologies. In NLP, this often means that a workflow will leverage both symbolic and machine learning techniques.\n\
+        Knowledged Based AI - Knowledge-based systems (KBs) are a form of artificial intelligence (AI) designed to capture the knowledge of human experts to support decision-making and problem-solving.\n\
+        Metadata - Data that describes or provides information about other data.")
+    
+    
+glossary()
