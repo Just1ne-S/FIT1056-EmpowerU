7beb01d2a82d739866afdabb7c65f97bdd23b454
Justin <114224750+Just1ne-S@users.noreply.github.com>
2024-10-21 12:23:42 +1100

    Add files via upload

---
+++ EmpowerUCode/app/compiled_adjust_lines.py
@@ -0,0 +1,8 @@
+from app.adjust_lines import adjust_lines
+
+def compiled_adjust_lines():
+    adjust_lines("./data/receptionist_login_info.txt")
+    adjust_lines("./data/user_login_info.txt")
+    adjust_lines("./data/activation_code_user.txt")
+    adjust_lines("./data/activation_code_receptionist.txt")
+    adjust_lines("./data/Python/python_progress.txt")
