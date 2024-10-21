c64b4c776ad94aaa6ea73d69659acc1cf3c6525d
Justin <114224750+Just1ne-S@users.noreply.github.com>
2024-10-21 19:29:17 +1100

    Update info_security_homepage.py

--- EmpowerUCode/interfaces/Information_Security/info_security_homepage.py
+++ EmpowerUCode/interfaces/Information_Security/info_security_homepage.py
@@ -4,6 +4,7 @@ from interfaces.content_buttons import ContentButton
 from interfaces.back_button import BackButton
 from interfaces.display_label import DisplayLabel
 from interfaces.Information_Security.info_security_content import InfoSecurityContent
+from app.quiz_ui import QuizUI
 
 class InfoSecurityHP(tk.Frame):
     def __init__(self,master,master_previous,user):
@@ -19,18 +20,19 @@ class InfoSecurityHP(tk.Frame):
         self.path_7 = "7.Ethical and Legal\n\
 Considerations in Information Security"
         self.path_8 = "8.Conclusion"
+        self.path_9 = "9.Quiz"
         self.user = user
         self.user_id = self.user.user_id
         self.pressed_list = []
-        self.content_buttons = ContentButton(self.master,self.path_1,self.path_2,self.path_3,self.path_4,self.path_5,self.path_6,self.path_7,self.path_8)
-        self.button_list = [self.content_buttons.topic_1,self.content_buttons.topic_2,self.content_buttons.topic_3,self.content_buttons.topic_4,self.content_buttons.topic_5,self.content_buttons.topic_6,self.content_buttons.topic_7,self.content_buttons.topic_8] 
+        self.content_buttons = ContentButton(self.master,self.path_1,self.path_2,self.path_3,self.path_4,self.path_5,self.path_6,self.path_7,self.path_8,self.path_9)
+        self.button_list = [self.content_buttons.topic_1,self.content_buttons.topic_2,self.content_buttons.topic_3,self.content_buttons.topic_4,self.content_buttons.topic_5,self.content_buttons.topic_6,self.content_buttons.topic_7,self.content_buttons.topic_8,self.content_buttons.topic_9] 
         with open("./data/Information_Security/information_security_progress.txt","r") as rf:
                 self.lines = rf.readlines()
                 self.lines = [line.strip() for line in self.lines]
                 for user_index,line in enumerate(self.lines):
-                    user_id,bool1,bool2,bool3,bool4,bool5,bool6,bool7,bool8 = line.split(",")
+                    user_id,bool1,bool2,bool3,bool4,bool5,bool6,bool7,bool8,bool9 = line.split(",")
                     if self.user_id == user_id: 
-                       self.pressed_list = [bool1,bool2,bool3,bool4,bool5,bool6,bool7,bool8]
+                       self.pressed_list = [bool1,bool2,bool3,bool4,bool5,bool6,bool7,bool8,bool9]
                        self.user_index = user_index
 
         for i in range(len(self.pressed_list)):
@@ -69,16 +71,16 @@ protect yourself and others in the digital world.\n\
 Every challenge is an opportunity to strengthen-\n\
 your skills and enhance your knowledge!"
                 ,directory=None,role="Text",rel_x=.2,rel_y=.7,font=("Arial Bold",12))  
-        self.content_buttons.topic_8.config(command=self.check_eighth_button)
+        self.content_buttons.topic_9.config(command=self.check_ninth_button)
         
-        
+        self.quiz_ui = QuizUI(master=self.master,master_previous=self,back_button_text="Back to Information Security Homepage",quiz_file_path="./data/quizzes/infosec_quiz.txt",is_interactive=True)
                 
     def activate_button(self,index):
         self.button_list[index].config(state="normal")
         self.pressed_list[index] = "False"
         self.show_content(index-1)
         count = self.pressed_list.count("False")
-        self.progress = 100/8*count
+        self.progress = 100/9*count
         self.progressbar.progress_value.set(self.progress)
         try:
             wf = open("./data/Information_Security/information_security_progress.txt","w")
@@ -91,12 +93,11 @@ your skills and enhance your knowledge!"
             print("Make sure that you have the file python_progress.txt in the data folder")
 
     
-    def check_eighth_button(self):
+    def check_ninth_button(self):
         self.pressed_list[0] = "False"   
         count = self.pressed_list.count("False")
-        self.progress = 100/8*count
+        self.progress = 100/9*count
         self.progressbar.progress_value.set(self.progress)
-        self.show_content(7)
         try:
             wf = open("./data/Information_Security/information_security_progress.txt","w")
             new_line = [self.user_id] + self.pressed_list
@@ -104,6 +105,13 @@ your skills and enhance your knowledge!"
             self.lines[self.user_index] = new_line
             for line in self.lines:
                 wf.write(f"{line}\n")
+        except FileNotFoundError:
+            print("Make sure that all files are present in the data folder.")
+        try:
+            self.quiz_ui = QuizUI(master=self.master,master_previous=self,back_button_text="Back to Information Security Homepage",quiz_file_path="./data/quizzes/infosec_quiz.txt",is_interactive=True)
+            self.quiz_ui.place_frame()
+            self.quiz_ui.back_button(rel_x=.118,rel_y=.95)
+            self.forget_homepage()
         except FileNotFoundError:
             print("Make sure that all files are present in the data folder.")
 
@@ -120,6 +128,7 @@ your skills and enhance your knowledge!"
         self.place(relx=.5,rely=.1,anchor=tk.CENTER)
         self.content_buttons.show_buttons()
         self.backbutton.place(relx=.08,rely=.95,anchor=tk.CENTER)
+        self.quiz_ui.back_button_quiz.forget_button()
         self.img.show_label()
         self.label.show_label()
 
