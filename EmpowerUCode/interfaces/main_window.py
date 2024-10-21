6060134e48d37b55101350b39823b4ff9dcf2bb2
Justin <114224750+Just1ne-S@users.noreply.github.com>
2024-10-21 13:28:50 +1100

    Update main_window.py

--- EmpowerUCode/interfaces/main_window.py
+++ EmpowerUCode/interfaces/main_window.py
@@ -2,7 +2,7 @@ import tkinter as tk
 
 from interfaces.homepage import HomePage
 from interfaces.sign_up_page import SignUpPage
-from app.adjust_lines import adjust_lines
+from interfaces.about_us_button import AboutUsButton
 
 class MW(tk.Tk):
     
@@ -12,15 +12,18 @@ class MW(tk.Tk):
         super().geometry(f"{width}x{height}")
         super().iconphoto(True,tk.PhotoImage(file="./images/EmpowerU Icon.png"))
 
-        self.homepage = HomePage(master=self, image_path="./images/EmpowerU Logo.png")
+        self.about_us_button = AboutUsButton(master=self)
+        self.homepage = HomePage(master=self, image_path="./images/EmpowerU Logo.png",about_us_button=self.about_us_button)
         self.sign_up_page = SignUpPage(master=self)
         self.show_homepage()
 
     def show_homepage(self):
         self.sign_up_page.place_forget()
         self.homepage.place(relx=.5, rely=.5, anchor=tk.CENTER)
+        self.about_us_button.show_button()
 
     def hide_homepage(self):
+        self.about_us_button.hide_button()
         self.homepage.place_forget()
 
     def show_sign_up_page(self):
