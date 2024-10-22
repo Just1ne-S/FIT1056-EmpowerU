bb686e3c36dd48740fc01fdf51e5030c7cc3e9ff
Afraz-Yes <81513625+Afraz-Yes@users.noreply.github.com>
2024-10-22 14:04:03 +1100

    Update Receptionist.py
    Added contact_num

--- EmpowerUCode/app/Receptionist.py
+++ EmpowerUCode/app/Receptionist.py
@@ -11,14 +11,14 @@ class Receptionist(User):
             for line in lines:
                 user_id,first_name,last_name,contact_num,username,password,code = line.strip().split(delimiter)
                 if input_username == username and input_password == password:
-                    return Receptionist(user_id,first_name,last_name,username,password,code)
+                    return Receptionist(user_id,first_name,last_name,contact_num,username,password,code)
             else:
                 return None
         else:
             print(f"Please check subdirectory and file {path} exists.")
 
-    def __init__(self,user_id,first_name,last_name,username,password,code):
-        super().__init__(user_id,first_name,last_name,username,password,code)
+    def __init__(self,user_id,first_name,last_name,contact_num,username,password,code):
+        super().__init__(user_id,first_name,last_name,contact_num,username,password,code)
 
 if __name__ == "__main__":
     pass
